c31d74c4202a06e07b1892fb6380566566ea8d73
Aaron McCollum <mccollumaarons@gmail.com>
2023-11-10 15:31:13 -0500

    added buttons and connected JS code

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Rolling dice function
 let rollDice = (side) => {
     let randomNum = Math.floor(Math.random() * side) + 1;
-    return randomNum;
+    let target = document.querySelector(".result-area");
+    target.textContent = randomNum;
 };
 
+let resetDice = () => {
+    let target = document.querySelector(".result-area");
+    target.textContent = "";
+}
